2eb04a6df42743ad184abd096eac48e98246c404
YUZ <107902463+yuz0517@users.noreply.github.com>
2022-10-13 12:19:09 +0900

    Create 10869.cpp

---
+++ backjoon/Steps/01/10869.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+//, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오. 
+int main(){
+    int A=0,B=0;
+    cin>>A>>B;
+    cout<<A+B<<endl;
+    cout<<A-B<<endl;
+    cout<<A*B<<endl;
+    cout<<A/B<<endl;
+    cout<<A%B<<endl;
+    return 0;
+}
