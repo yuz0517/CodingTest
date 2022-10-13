5ee1a1ffb10611930e2b0b155df7695186e589f7
YUZ <107902463+yuz0517@users.noreply.github.com>
2022-10-13 12:08:37 +0900

    Create 1001.cpp

---
+++ backjoon/Steps/01/1001.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1,num2,result=0;
+    std::cin >> num1 >> num2;
+    result = num1-num2;
+    std::cout<<result;
+
+    return 0;
+}
