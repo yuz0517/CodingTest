43fc8add40fece1db5c8980a74d4709699e4930e
YUZ <107902463+yuz0517@users.noreply.github.com>
2023-02-13 00:04:16 +0900

    Update 10950_a+b=3.py
    수정완료

--- backjoon/Steps/03/10950_a+b=3.py
+++ backjoon/Steps/03/10950_a+b=3.py
@@ -1,14 +1,14 @@
 import sys
 n = sys.stdin.readline()
- 
+n = int(n)
 arr  = []
 
-for i in range(n+1):
+for i in range(n):
   a,b = sys.stdin.readline().split()
   
   a = int(a)
   b = int(b)
   arr.append(a+b)
 
-for i in range(n+1):
-  print(arr)
+for i in range(n):
+  print(arr[i])
