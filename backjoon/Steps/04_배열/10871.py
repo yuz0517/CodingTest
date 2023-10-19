a86eb57c810d9aff2a25f56513a7ad8c803ad66d
YUZ <107902463+yuz0517@users.noreply.github.com>
2023-10-19 21:50:18 +0900

    Create 10871.py

---
+++ backjoon/Steps/04_배열/10871.py
@@ -0,0 +1,11 @@
+#정수 N개로 이루어진 수열 A와 정수 X가 주어진다. 이때, A에서 X보다 작은 수를 모두 출력하는 프로그램을 작성하시오.
+N,X = map(int,input().split())
+A=[]
+A=(input().split())
+New=[]
+for i in A:
+    if int(i) < X:
+        New.append(i)
+    else:
+        continue
+print(*New, sep=' ')
